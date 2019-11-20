b0cb5f7f24f09bdd19793f7a4c14ece85951b875
Monica Gupta <monica.gupta.ms@gmail.com>
2019-11-19 19:39:18 -0500

    code for input done

--- read_input.cpp
+++ read_input.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
 #include <iomanip>  // for setw() and ws
 #include <string>
 #include <fstream>
 #include <cstdlib>
-#include<vector>
+#include <sstream>
 
 using namespace std;
 
@@ -11,45 +10,44 @@ using namespace std;
 int main()
 {
     string line;
- 
     ifstream tsp_inp;
     tsp_inp.open("DATA/Atlanta.tsp");
-    int count_line = 0;
+    int count_line = 0, dim = 0;
     if (tsp_inp.is_open())
-  {
-        while (getline (tsp_inp,line) )
     {
+        while (getline(tsp_inp,line) )
+        {
             count_line += 1;
             if(count_line == 3)
             {
-                for(int i = 12; line[i]!='\n';i++)
-                {
-                    
-                }
+                istringstream iss(line);
+                string s;
+                if (iss >> s >> dim)
+                break;
             }
-
-
-
+        }
     }
-    count_line = 0
-    double coord[dim][dim];
+    double coord[dim][2];
+    int i=0;
     if (tsp_inp.is_open())
     {
-    while (getline (tsp_inp,line) )
-    {
-            count_line += 1;
-            if(count_line == 12)
-            {
-                for(int i = 12; line[i]!='\n';i++)
+        while (getline(tsp_inp,line) )
+        {
+                count_line += 1;
+                if((count_line >= 6) && (count_line < (dim+6)))
                 {
-                    
+                    istringstream iss(line);
+                    int a;
+                    double b,c;
+                    if (iss >> a >> b >> c) {
+                        coord[i][0] = b;
+                        coord[i][1] = c;
+                        i++;
+                    }
                 }
-            }
-
+        }
     }
-
+    for(i=0;i<dim;i++)
+        printf("%d %lf %lf\n",i, coord[i][0], coord[i][1]);
     tsp_inp.close();
-
-
-  }
 }
