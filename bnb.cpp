7d27479da71a1e4a3336938c9106d2d481aeed96
Monica Gupta <monica.gupta.ms@gmail.com>
2019-11-25 01:01:29 -0500

    added code to create solution and trace files

--- bnb.cpp
+++ bnb.cpp
@@ -1,15 +1,42 @@
 #include <iomanip>  // for setw() and ws
 #include <string>
-#include <fstream>
+#include <fstream> // for file handling
 #include <cstdlib>
-#include <sstream>
+#include <sstream> // for reading file
 #include <cmath>
 #include <cstring>
 #include <cstdio>
 #include <iostream>
-#include <climits>
+#include <climits> // for INT_MAX
+#include <chrono> // for time
 
 using namespace std;
+
+// function to write new improved solutions when found
+void writeToOutputFile(string traceFileName, string solFileName, int bestWeight, chrono::high_resolution_clock::time_point startTime, int curr_path[], int dim)
+{
+    ofstream tracefile;
+    ofstream solfile;
+
+    // open trace file in append mode
+    tracefile.open(traceFileName, ios_base::app);
+    solfile.open(solFileName);
+
+    // find the current time
+    chrono::high_resolution_clock::time_point currTime = chrono::high_resolution_clock::now(); 
+
+    // find time difference in seconds to measure execution time rounded to 2 decimal places
+    float diff = round((chrono::duration_cast<chrono::microseconds>(currTime - startTime).count())/10000.0)/100.0;
+
+    // write results to output files
+    tracefile<<diff<<", "<<bestWeight<<"\n";
+    solfile<<bestWeight<<"\n";
+    for(int i=0;i<dim;i++)
+        solfile<<curr_path[i]<<" ";
+    solfile<<curr_path[0];
+    solfile.close();
+    tracefile.close();
+}
 
 class TSP_BNB
 {   
@@ -62,8 +89,7 @@ class TSP_BNB
                 second = first; 
                 first = adj[i][j]; 
             } 
-            else if (adj[i][j] <= second && 
-                     adj[i][j] != first) 
+            else if (adj[i][j] <= second && adj[i][j] != first) 
                 second = adj[i][j]; 
         } 
         return second; 
@@ -73,7 +99,7 @@ class TSP_BNB
     // curr_weight-> weight of the path so far 
     // level-> current level in the tree
     // curr_path[] -> current solution so far 
-    void BNBRecursive(int** adj, int curr_bound, int curr_weight, int level, int curr_path[], int *final_res, bool *visited, int *final_path) 
+    void BNBRecursive(int** adj, int curr_bound, int curr_weight, int level, int curr_path[], int *final_res, bool *visited, int *final_path, chrono::high_resolution_clock::time_point startTime, string traceFilePath, string solFilePath) 
     { 
         // case when we have covered all the nodes once
         if (level==N) 
@@ -86,6 +112,7 @@ class TSP_BNB
             { 
                 copyToFinal(curr_path, final_path); 
                 *final_res = curr_res; 
+                writeToOutputFile(traceFilePath, solFilePath, curr_res, startTime, curr_path, N);
             }                
             return; 
         } 
@@ -94,20 +121,17 @@ class TSP_BNB
         for (int i=0; i<N; i++) 
         { 
             // consider next vertex if it is not the vertex itself and not visited
-            if (adj[curr_path[level-1]][i] != 0 && 
-                visited[i] == false) 
+            if (adj[curr_path[level-1]][i] != 0 && visited[i] == false) 
             { 
                 int temp = curr_bound; 
                 curr_weight += adj[curr_path[level-1]][i]; 
       
                 //finding curr_bound for level 2 
                 if (level==1) 
-                  curr_bound -= ((firstMin(adj, curr_path[level-1]) + 
-                                 firstMin(adj, i))/2); 
+                  curr_bound -= ((firstMin(adj, curr_path[level-1]) + firstMin(adj, i))/2); 
                 //curr_bound for remaininig levels
                 else
-                  curr_bound -= ((secondMin(adj, curr_path[level-1]) + 
-                                 firstMin(adj, i))/2); 
+                  curr_bound -= ((secondMin(adj, curr_path[level-1]) + firstMin(adj, i))/2); 
       
                 // curr_bound + curr_weight is the lower bound for current node 
                 // if current lower bound < final_res, we need to explore the node further 
@@ -117,8 +141,7 @@ class TSP_BNB
                     visited[i] = true; 
       
                     // call BNBRecursive for the next level 
-                    BNBRecursive(adj, curr_bound, curr_weight, level+1, 
-                           curr_path, final_res, visited, final_path); 
+                    BNBRecursive(adj, curr_bound, curr_weight, level+1, curr_path, final_res, visited, final_path, startTime, traceFilePath, solFilePath); 
                 } 
       
                 // else we prune the node by resetting all changes
@@ -133,7 +156,7 @@ class TSP_BNB
     } 
       
     // set first node as root, initialise curr_path, find initial bound and call the recursive fn 
-    void BNB(int** adj, int *final_res, bool *visited, int *final_path) 
+    void BNB(int** adj, int *final_res, bool *visited, int *final_path, chrono::high_resolution_clock::time_point startTime, string traceFilePath, string solFilePath) 
     {   
         int curr_path[N+1]; 
       
@@ -145,84 +168,66 @@ class TSP_BNB
       
         //find initial bound 
         for (int i=0; i<N; i++) 
-            curr_bound += (firstMin(adj, i) + 
-                           secondMin(adj, i)); 
+            curr_bound += (firstMin(adj, i) + secondMin(adj, i)); 
       
         // round lower bound to an integer 
-        curr_bound = (curr_bound&1)? curr_bound/2 + 1 : 
-                                     curr_bound/2; 
+        curr_bound = (curr_bound&1)? curr_bound/2 + 1 : curr_bound/2; 
       
         // mark the vertex as visited and add it to the curr_path
         visited[0] = true; 
         curr_path[0] = 0; 
       
         // call the BNB recursive function
-        BNBRecursive(adj, curr_bound, 0, 1, curr_path, final_res, visited, final_path); 
+        BNBRecursive(adj, curr_bound, 0, 1, curr_path, final_res, visited, final_path, startTime, traceFilePath, solFilePath); 
     } 
 };
 
-// function to find adjacency matrix given total number vertices and their coordinates
-int **findAdj(double **coord, int dim)
-{
-    // creating the adjacency matrix
-    int** adj = new int*[dim];
-    for (int i = 0; i < dim; ++i)
-        adj[i] = new int[dim];
-
-    // assigning values to the adjacency matrix
-    for (int i = 0; i < dim; i++)
-        for (int j = 0; j <= i; j++){
-            // set distance to 0 for a node to itself
-            if ( i == j)
-                adj[i][j] = 0;
-            // else use euclidean distance
-            else
-                adj[j][i] = adj[i][j] = round(sqrt((coord[i][0]-coord[j][0])*(coord[i][0]-coord[j][0])+(coord[i][1]-coord[j][1])*(coord[i][1]-coord[j][1]))); 
-        }
-    return adj;
-}
-
-// function to read the input file and find the number of vertices and return their coordinates
-double ** readFile(int *dim, string file_name)
+// function to find the number of vertices
+int getDim(string filePath)
 {
     string line;
     ifstream tsp_inp;
-    tsp_inp.open(file_name);
-    int count_line = 0, i;
-
-    // finding the number of vertices
+    tsp_inp.open(filePath);
+    int count_line = 0, dim;
     if (tsp_inp.is_open())
     {
-        while (getline(tsp_inp,line) )
+        while (getline(tsp_inp, line))
         {
             count_line += 1;
-            if(count_line == 3)
+            if (count_line == 3)
             {
                 istringstream iss(line);
                 string s;
-                iss >> s >> *dim;
+                if (iss >> s >> dim)
                 break;
             }
         }
     }
+    return dim;
+}
 
-    // creating the coordinates matrix
-    double** coord = new double*[*dim];
-    for(i = 0; i < *dim; ++i)
+// function to get the adjacency matrix and coordinates of vertices
+int** getAdjMatrix(string filePath, int dim)
+{
+    string line;
+    ifstream tsp_inp;
+    tsp_inp.open(filePath);
+    int count_line = 0, i;
+
+    double** coord = new double*[dim];
+    for (i = 0; i < dim; ++i)
         coord[i] = new double[2];
-    i=0;
-
-    // assigning values to the coordinates matrix
+    i = 0;
     if (tsp_inp.is_open())
     {
-        while (getline(tsp_inp,line) )
+        while (getline(tsp_inp, line))
         {
             count_line += 1;
-            if ((count_line >= 6) && (count_line < (*dim+6)))
+            if ((count_line >= 6) && (count_line < (dim + 6)))
             {
                 istringstream iss(line);
                 int a;
-                double b,c;
+                double b, c;
                 if (iss >> a >> b >> c) {
                     coord[i][0] = b;
                     coord[i][1] = c;
@@ -232,28 +237,46 @@ double ** readFile(int *dim, string file_name)
         }
     }
 
-    // close the file
     tsp_inp.close();
-    return coord;
+
+    int** adj = new int*[dim];
+
+    for (i = 0; i < dim; ++i)
+        adj[i] = new int[dim];
+
+    for (i = 0; i < dim; i++)
+        for (int j = 0; j <= i; j++) {
+            if (i == j)
+                adj[i][j] = 0;
+            else
+                adj[j][i] = adj[i][j] = round(sqrt((coord[i][0] - coord[j][0])*(coord[i][0] - coord[j][0]) + (coord[i][1] - coord[j][1])*(coord[i][1] - coord[j][1])));
+        }
+    return adj;
 }
 
 int main()
 {
-    int *dim;
-    string file_name = "DATA/Roanoke.tsp";
-    // calling the function to find number of vertices and their coordinates
-    double **coord = readFile(dim, file_name);
+    // making note of start time of execution
+    chrono::high_resolution_clock::time_point startTime = chrono::high_resolution_clock::now();
+    string inputFilePath = "DATA/Atlanta.tsp";
+    string traceFilePath = "Atlanta.trace";
+    string solFilePath = "Atlanta.sol";
+    // calling the function to find number of vertices
+    int V = getDim(inputFilePath);
     
-    // calling function to find adjacency matrix
-    int **adj = findAdj(coord,*dim);
+    // calling function to find coordinates of vertices and adjacency matrix
+    int **adj = getAdjMatrix(inputFilePath, V);
 
     //calling BNB
-    TSP_BNB obj(*dim);
-    obj.BNB(adj, &obj.final_res, obj.visited, obj.final_path); 
+    TSP_BNB obj(V);
+    obj.BNB(adj, &obj.final_res, obj.visited, obj.final_path, startTime, traceFilePath, solFilePath); 
 
     printf("Minimum cost : %d\n", obj.final_res); 
     printf("Path Taken : "); 
     for (int i=0; i<=obj.N; i++) 
         printf("%d ", obj.final_path[i]); 
+    chrono::high_resolution_clock::time_point currTime = chrono::high_resolution_clock::now();
+    float diff = (chrono::duration_cast<chrono::microseconds>(currTime-startTime).count())/1000000.0;
+    cout<<diff;
     return 0;
 }
