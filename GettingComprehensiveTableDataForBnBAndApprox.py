ea899c01ed076a19463fa464520079468249b1d1
Sanmeshkumar Udhayakumar <sanmesh1@gmail.com>
2019-12-03 18:52:46 -0500

    changed some plots

--- GettingComprehensiveTableDataForBnBAndApprox.py
+++ GettingComprehensiveTableDataForBnBAndApprox.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 import pandas as pd
 import numpy as np
 from PlotterForSQD import Plotter
-
+import sys
 
 class QRTD(Plotter):
     def __init__(self, outdir, target_loc, algo, solpath="DATA/solutions.csv",
@@ -34,8 +34,8 @@ class QRTD(Plotter):
         if len(tps) == 0:
             raise ValueError('outdir must contain trace files')
 
-        # cutoff in secs
-        self.cutoff = int(tps[0].split('_')[2])
+##        # cutoff in secs
+##        self.cutoff = int(tps[0].split('_')[2])
 
         # get optimal value from provided solutions for trace instance
         df_sol = pd.read_csv(solpath)
@@ -122,68 +122,45 @@ class QRTD(Plotter):
         #print(dataNp)
         lastInd = dataNp.shape[0]-1
         lastTrialNum = dataNp[lastInd, 0]
+##        np.set_printoptions(threshold=sys.maxsize)
+##        print("dataNp",dataNp)
 
-        #initialize array to store relative quality
-        relQuality = np.zeros((times.shape[0], lastTrialNum)) #times are rows, trial number are columns
-
-        #add a small time delta to times column so it isnt mistaken as 
-        dataNp[:,1]+= 0.000001
-##        print("dataNp", dataNp)
-        
+        meanRunTime = 0
+        meanSolQual = 0
+        meanRelSolQual = 0
         for i in range(1,lastTrialNum+1): #i goes from 1 to 10, not start from 0
             trialInd = np.argwhere(dataNp[:,0]==i)
             trialInd = np.squeeze(trialInd)
-##            print(trial)
+##            print("dataNp[trialInd]",dataNp[trialInd])
+##            print(trialInd)
+##            print("trialInd: ",trialInd)
+##            endIndex = trialInd[trialInd.shape[0] -1]
+##            print("endIndex: ",endIndex)
             trialData = dataNp[trialInd] #have data for a specific trial i where i is 1 to 10
-##            print("trialData[:,3]")
-##            print(trialData[:,3])
-
-            #for a given time for each trial, find the first index <= time constraint
-            for timeInd in range(times.shape[0]):
-                boolCond = np.argwhere(trialData[:,1] <= times[timeInd])
-##                print(boolCond)
-                if boolCond.shape[0] == 0: #this checks if there was no solution that had less than the alotted time
-                    relQuality[timeInd, i-1] = 10000.0 #assign some high relative error, so that when we compare to rsq in x axis to calculate appropriate p(solve, it will always fail) the threshold
-                else:
-                    indTime = boolCond[boolCond.shape[0]-1][0]
-##                    print("indTime ", indTime)
-                    rq = trialData[indTime, 3]
-##                    print("rq", rq)
-                    relQuality[timeInd, i-1] = rq
-##        print("relQuality")
-##        print(relQuality)
-
-        #x = set of predecided relative solution qualities
-        x = np.arange(0,maxSolQualPerc, solutQualPercInterval)
-        X = np.array([x.copy() for _ in range(times.shape[0])])
-        Y = np.zeros((times.shape[0],x.shape[0]))
-        for timeInd in range(times.shape[0]):
-            for rqXInd in range(x.shape[0]):
-                psolve = np.mean(relQuality[timeInd, :]*100 <=  x[rqXInd]) #multiplying by 100 since its % comparison
-                Y[timeInd, rqXInd] = psolve
-##        print("X ", X)
-##        print("Y ", Y)
-##        print(type(Y))
-
-        if should_show or should_save:
-            if labels is None:
-                labels = [f'{re} s' for re in times]
-##                print(labels)
-            if title is None:
-                title = f'Qualified RTD ({self.target_loc})'
-
-            self.plot(X, Y, labels=labels, xax_label=xax_label,
-                      yax_label=yax_label, colors=colors, title=title,
-                      should_show=should_show, should_save=should_save,
-                      save_path=save_path,xscale=xscale, linestyles = linestyles)
-
+##            print(trialData)
+            meanRunTime += trialData[1]
+            meanSolQual += trialData[2]
+            meanRelSolQual += trialData[3]
+            print(trialData[3])
+        meanRunTime /= 1
+        meanSolQual /= 1
+        meanRelSolQual /= 1
+        
+        print(meanRunTime, meanSolQual, meanRelSolQual)
+        return [meanRunTime, meanSolQual, meanRelSolQual]
 if __name__ == '__main__':
     """
     Example: QRTD plot for simulated annealing NYC results
     """
-    City = 'Denver'
-    Algorithm = 'LS1'
-    plotter = QRTD('output', City, Algorithm)
-    #plotter.build(times=np.array([0.1,0.3,1, 3.2,10]), colors=['b', 'g', 'r', 'c', 'y'], linestyles = ['-','--','-.',':', '-'], maxSolQualPerc = 40, solutQualPercInterval = 0.1, should_show=True, xscale=None)
-    plotter.build(title = 'SQD_' + City +'_' + Algorithm,times=np.array([0.1,0.6, 0.7, 1, 3.2,10]), colors=['g', 'r', 'c', 'y', 'b', 'g'], linestyles = ['--','-.',':', '-','-.',':'], maxSolQualPerc = 40, solutQualPercInterval = 0.1, should_show=True, xscale=None)
+    City = ['Atlanta', 'Berlin', 'Boston', 'Champaign', 'Cincinnati', 'Denver', 'NYC',  'Philadelphia', 'Roanoke', 'SanFrancisco', 'Toronto', 'UKansasState', 'UMissouri']
+    Algorithm = 'BnB'
+    out = []
+    for city1 in City:
+        print(city1)
+        plotter = QRTD('BnB_out', city1, Algorithm)
+        #plotter.build(times=np.array([0.1,0.3,1, 3.2,10]), colors=['b', 'g', 'r', 'c', 'y'], linestyles = ['-','--','-.',':', '-'], maxSolQualPerc = 40, solutQualPercInterval = 0.1, should_show=True, xscale=None)
+        buf = plotter.build(title = 'SQD_' + city1 +'_' + Algorithm,times=np.array([0.1,0.6, 0.7, 1, 3.2,10]), colors=['g', 'r', 'c', 'y', 'b', 'g'], linestyles = ['--','-.',':', '-','-.',':'], maxSolQualPerc = 40, solutQualPercInterval = 0.1, should_show=True, xscale=None)
+        out.append(buf)
+    out = np.array(out)
+    np.savetxt('test.csv', out, delimiter=',')
     #0.1, 0.6, 
